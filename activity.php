07189ca86cd68d22c667c40c75affc39046bb2fb
karan singh <karan.singh@ebslon.com>
2021-11-20 19:18:48 +0530

    commited activity page

--- activity.php
+++ activity.php
@@ -14,137 +14,47 @@
     <section class="activity-page mb-80">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-12 col-md-4">
-                    <ul>
-                        <li>
-                            <div class="activity-inner">
-                                <div class="overflow-hidden">
-                                    <img src="./src/images/activity1.jpg" alt="">
-                                </div>
-                                <div class="activity-absolute">
-                                    <p>Save Girl Child</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="activity-inner">
-                                <div class="overflow-hidden">
-                                    <img src="./src/images/activity2.jpg" alt="">
-                                </div>
-                                <div class="activity-absolute">
-                                    <p>Family Planning</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="activity-inner">
-                                <div class="overflow-hidden">
-                                    <img src="./src/images/activity3.jpg" alt="">
-                                </div>
-                                <div class="activity-absolute">
-                                    <p>Domestic Violence</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="activity-inner">
-                                <div class="overflow-hidden">
-                                    <img src="./src/images/activity4.jpg" alt="">
-                                </div>
-                                <div class="activity-absolute">
-                                    <p>Early Marriage</p>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
+                <div class="col-12 col-sm-6 col-md-4">
+                    <div class="activity-inner">
+                        <div class="overflow-hidden">
+                            <img src="./src/images/activity1.jpg" alt="">
+                        </div>
+                        <div class="activity-absolute">
+                            <p>Save Girl Child</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-12 col-md-4">
-                    <ul>
-                        <li>
-                            <div class="activity-inner">
-                                <div class="overflow-hidden">
-                                    <img src="./src/images/activity1.jpg" alt="">
-                                </div>
-                                <div class="activity-absolute">
-                                    <p>Save Girl Child</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="activity-inner">
-                                <div class="overflow-hidden">
-                                    <img src="./src/images/activity2.jpg" alt="">
-                                </div>
-                                <div class="activity-absolute">
-                                    <p>Family Planning</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="activity-inner">
-                                <div class="overflow-hidden">
-                                    <img src="./src/images/activity3.jpg" alt="">
-                                </div>
-                                <div class="activity-absolute">
-                                    <p>Domestic Violence</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="activity-inner">
-                                <div class="overflow-hidden">
-                                    <img src="./src/images/activity4.jpg" alt="">
-                                </div>
-                                <div class="activity-absolute">
-                                    <p>Early Marriage</p>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
+                <div class="col-12 col-sm-6 col-md-4">
+                    <div class="activity-inner">
+                        <div class="overflow-hidden">
+                            <img src="./src/images/activity2.jpg" alt="">
+                        </div>
+                        <div class="activity-absolute">
+                            <p>Family Planning</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-12 col-md-4">
-                    <ul>
-                        <li>
-                            <div class="activity-inner">
-                                <div class="overflow-hidden">
-                                    <img src="./src/images/activity1.jpg" alt="">
-                                </div>
-                                <div class="activity-absolute">
-                                    <p>Save Girl Child</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="activity-inner">
-                                <div class="overflow-hidden">
-                                    <img src="./src/images/activity2.jpg" alt="">
-                                </div>
-                                <div class="activity-absolute">
-                                    <p>Family Planning</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="activity-inner">
-                                <div class="overflow-hidden">
-                                    <img src="./src/images/activity3.jpg" alt="">
-                                </div>
-                                <div class="activity-absolute">
-                                    <p>Domestic Violence</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="activity-inner">
-                                <div class="overflow-hidden">
-                                    <img src="./src/images/activity4.jpg" alt="">
-                                </div>
-                                <div class="activity-absolute">
-                                    <p>Early Marriage</p>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
+                <div class="col-12 col-sm-6 col-md-4">
+                    <div class="activity-inner">
+                        <div class="overflow-hidden">
+                            <img src="./src/images/activity3.jpg" alt="">
+                        </div>
+                        <div class="activity-absolute">
+                            <p>Domestic Violence</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4">
+
+                    <div class="activity-inner">
+                        <div class="overflow-hidden">
+                            <img src="./src/images/activity4.jpg" alt="">
+                        </div>
+                        <div class="activity-absolute">
+                            <p>Early Marriage</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
