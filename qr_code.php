df00f9dd0b483613511d26fdbc828439eb418388
karan singh <karan.singh@ebslon.com>
2021-11-22 12:45:47 +0530

    commit of all pages again

--- qr_code.php
+++ qr_code.php
@@ -6,12 +6,12 @@
     <section class="qr-page position-relative">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-12 col-md-6">
+                <div class="col-12 col-sm-6">
                     <div class="title-section">
                         <h4>Scan QR Code to proceed your payment</h4>
                     </div>
                 </div>
-                <div class="col-12 col-md-6">
+                <div class="col-12 col-sm-6">
                     <div class="donate-right">
                         <img src="./src/images/qr-code.jpg" alt="">
                     </div>
