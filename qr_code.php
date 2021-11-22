1aaee620669d57ffde27cb93a5040ddde82259e0
karan singh <karan.singh@ebslon.com>
2021-11-22 11:06:29 +0530

    commit of qr-code page

---
+++ qr_code.php
@@ -0,0 +1,25 @@
+<?php include("./header-css.php");?>
+<?php include("./header.php");?>
+
+<main>
+
+    <section class="qr-page position-relative">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-6">
+                    <div class="title-section">
+                        <h4>Scan QR Code to proceed your payment</h4>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="donate-right">
+                        <img src="./src/images/qr-code.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
+<?php include("./footer.php");?>
+<?php include("./footer-scripts.php");?>
